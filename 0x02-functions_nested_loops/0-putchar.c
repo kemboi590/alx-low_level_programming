10aa1ee9f7ef81c3a39b3432f404af903dbe5f6f
kemboi590 <bkemboi590@gmail.com>
2022-12-13 01:16:05 -0800

    0 _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,6 +7,14 @@
 
 int main(void)
 {
-	printf("_putchar\n");
+	printf('_');
+	printf('p');
+	printf('u');
+	printf('t');
+	printf('c');
+	printf('h');
+	printf('a');
+	printf('r');
+	printf('\n');
 	return (0);
 }
