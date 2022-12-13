e71ba3c95da92a59c7c33ed32ba557d5cbc25121
kemboi590 <bkemboi590@gmail.com>
2022-12-13 01:57:32 -0800

    correction

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,16 +1,16 @@
 #ifndef main_h
 #define main_h
-int _putchar(char);
+char _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
 int _isalpha(int c);
 int print_sign(int n);
 int _abs(int);
-int print_last_digit(int);
+int print_last_digit(int nld);
 void jack_bauer(void);
 void times_table(void);
-int add(int, int);
+int add(int one, int two);
 void print_to_98(int n);
 void print_times_table(int n);
 #endif
