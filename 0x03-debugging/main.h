1f2ca8c3ea4b0921fce1e283571e031ad83fa20d
kemboi590 <bkemboi590>
2022-12-14 02:15:32 -0800

    correction

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,24 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+
 #include <stdio.h>
 
-/**
-* main - causes an infinite loop
-* Return: 0
-*/
+void positive_or_negative(int i);
+int largest_number(int a, int b, int c);
+void print_remaining_days(int month, int day, int year);
+int convert_day(int month, int day);
 
-int main(void)
-{
-	int i;
-
-	printf("Infinite loop incoming :(\n");
-
-	i = 0;
-
-	/*while (i < 10)*/
-	/*{*/
-	/*putchar(i);*/
-	/*}*/
-
-	printf("Infinite loop avoided! \\o/\n");
-
-	return (0);
-}
+#endif /* MAIN_H */
